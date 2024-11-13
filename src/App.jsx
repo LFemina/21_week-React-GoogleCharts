9be5239803bc06b1b55fa8148927b72d5d56c9da
lfemina <lfemina@yandex.ru>
2024-11-13 22:48:51 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './App.css';
+import ChartComponent from "./ChartComponent";
+
+const App = () => (
+  <div>
+    <h1>My React Google Chart</h1>
+    <ChartComponent />
+  </div>
+);
+
+export default App;
